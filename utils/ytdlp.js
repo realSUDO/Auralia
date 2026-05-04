16d2210a1bfde5ea515ddac82e4add9ff3028933
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-05-04 16:09:53 +0530

    add js-runtimes and remote-components for yt-dlp 2026

--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
-const YTDLP_EXTRA = ['--cookies', path.join(require('os').homedir(), 'yt-cookies.txt')];
+const YTDLP_EXTRA = [
+  '--cookies', path.join(require('os').homedir(), 'yt-cookies.txt'),
+  '--js-runtimes', 'node',
+  '--remote-components', 'ejs:github',
+];
 
 function ytdlpArgs(args) {
   return [...args, ...YTDLP_EXTRA];
