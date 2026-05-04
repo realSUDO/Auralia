8e7ea54dd901ff09cb97625d8f0d60f6a867b37d
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-05-04 17:09:40 +0530

    restore cookies + keep js-runtimes and remote-components

--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -1,4 +1,7 @@
+const path = require('path');
+
 const YTDLP_EXTRA = [
+  '--cookies', path.join(require('os').homedir(), 'yt-cookies.txt'),
   '--js-runtimes', 'node',
   '--remote-components', 'ejs:github',
 ];
