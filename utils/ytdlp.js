d62c2af873cc87e0b783838cf6fb01f46251bdfc
Sumit Vishwkarma <justmultiplythinks@gmail.com>
2026-05-04 17:21:04 +0530

    add optional YTDLP_PROXY env var support

--- utils/ytdlp.js
+++ utils/ytdlp.js
@@ -4,6 +4,7 @@ const YTDLP_EXTRA = [
   '--cookies', path.join(require('os').homedir(), 'yt-cookies.txt'),
   '--js-runtimes', 'node',
   '--remote-components', 'ejs:github',
+  ...(process.env.YTDLP_PROXY ? ['--proxy', process.env.YTDLP_PROXY] : []),
 ];
 
 function ytdlpArgs(args) {
